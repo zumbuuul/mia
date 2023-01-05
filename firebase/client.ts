5503246826868a32ca1c35474f02a39a2c973e6c
Lazar <melje11delje@gmail.com>
2023-01-05 18:31:29 +0100

    set up next+firebase for dev

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXGeVM4L2koxMe985HDjVoT0-b-FTz5Zo",
+  authDomain: "mia-dev-e5236.firebaseapp.com",
+  projectId: "mia-dev-e5236",
+  storageBucket: "mia-dev-e5236.appspot.com",
+  messagingSenderId: "553281811218",
+  appId: "1:553281811218:web:d0d2819d1098a21ca639cb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
