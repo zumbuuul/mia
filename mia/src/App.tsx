784f75518f61ed8f68b1c38299f54df5c607deac
lazar <krstiic.lazar@gmail.com>
2024-08-14 23:26:56 +0200

    merged autocomplete logic with tailwind setup successfully

--- mia/src/App.tsx
+++ mia/src/App.tsx
@@ -2,14 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { query, collection, getDocs, orderBy, limit, addDoc } from "firebase/firestore"
+import db from "../firebase/firebase.ts"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState('')
 
+  const handleQueryChange = (newQuery: string) => setSearchQuery(newQuery)
+
+  const fetch = async () => {
+    const ref = collection(db, "books")
+    const q = query(ref, orderBy(`searchIndex.sc`), limit(5))
+    const snap = await getDocs(q)
+    snap.forEach(doc => console.log(doc.data()))
+  }
+
+  const add = () => {
+    const data = {
+      title: searchQuery,
+      searchIndex: format(searchQuery)
+    }
+    addDoc(collection(db, "books"), data)
+  }
+
+  const format = (title: string) => {
+    const words = title.split('')
+    const searchIndex: { [key: string]: boolean } = {}
+
+    let prev = ''
+    for (const char of words) {
+      const key = prev + char
+      searchIndex[key] = true
+      prev = key
+    }
+    return searchIndex
+  }
+
+  fetch()
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="google.com" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -24,6 +58,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <input value={searchQuery} onChange={(e) => handleQueryChange(e.target.value)}></input>
+        <button onClick={() => { add() }}>dodaj</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
